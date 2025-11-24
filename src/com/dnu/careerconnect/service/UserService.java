d29fa8c930829b50390d929881f9409ae60037e9
thangnguyenviet-code <thangthangnguyenviet@gmail.com>
2025-11-24 12:14:42 +0700

    Delete src/com/dnu/careerconnect directory

--- src/com/dnu/careerconnect/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.dnu.careerconnect.service;
-
-import com.dnu.careerconnect.model.User;
-
-public interface UserService {
-    User register(User user);
-    User findByUsername(String username);
-}
